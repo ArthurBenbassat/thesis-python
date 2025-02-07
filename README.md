72852a9ac22d676bc1d8d64556b132b97167c292
Arthur Benbassat <arthur.benbassat@student.kdg.be>
2025-02-07 10:40:32 +0100

    kbo address & name in testproject

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 - Arthur Benbassat
 - VUB - Master Management
 
+
 ## PART 1 - get the address and for every company with their VAT number
+### Requirements
+- pandas
+- numpy
+- request
+- lxml
+- beautifulsoup4
+
+
+
 
--- getAddressAndNameByKbo.py
+++ getAddressAndNameByKbo.py
@@ -1,11 +1,37 @@
 import requests
 import pandas as pd
+from bs4 import BeautifulSoup
+
 url = "https://kbopub.economie.fgov.be/kbopub/zoeknummerform.html"
 
 data = pd.read_csv('testKboNummers.csv', delimiter=',')
-print(data.head())
 
-for index, row in data.iterrows():
-    request = requests.get(url, params={'nummer': row['ondernemingsnummer']})
-    print(request.status_code)
-#requests.get(url, params={nummer: value, }, args)
+
+
+def fetch_kbo_data(ondernemingsnummer):
+    response = requests.get(url, params={'nummer': ondernemingsnummer})
+
+    if response.status_code != 200:
+        return pd.NA, pd.NA
+
+    soup = BeautifulSoup(response.text, "lxml")
+
+    # Check of het nummer ongeldig is
+    if "Het nummer is ongeldig." in soup.text:
+        return pd.NA, pd.NA
+
+    # Get company name from
+    naam_tag = soup.find("td", string="Naam:")
+    naam = naam_tag.find_next("td").find(text=True, recursive=False).strip() if naam_tag else pd.NA
+
+    # Get Address
+    adres_tag = soup.find("td", string="Adres van de zetel:")
+    adres = adres_tag.find_next("td").text.strip().replace("\n", " ").replace("\xa0", " ") if adres_tag else pd.NA
+
+    return naam, adres
+
+
+data[['Naam', 'Adres']] = data['ondernemingsnummer'].apply(lambda num: pd.Series(fetch_kbo_data(num)))
+
+
+print(data.head())
