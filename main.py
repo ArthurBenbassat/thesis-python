b84c56df087592e6734b81f3e6474a3ed247112c
ArthurBenbassat <arthur.benbassat@gmail.com>
2025-03-26 14:15:50 +0100

    With real data getting place id from google

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ sys.path.append("Google Reviews")
 from getDataIdGoogle import get_place_id
 from getDetailsFromPlacesApi import get_details_from_places_api
 
-# with kbonummers adding address and company name (Dit voorbeeld met VUB, philip morris & café belga)
-data = pd.read_csv('testKboNummers.csv', delimiter=',')
-data[['Name', 'Address']] = data['ondernemingsnummer'].apply(lambda num: pd.Series(fetch_kbo_data(num)))
+
+data = pd.read_csv('Data/misdrijf_pubs.csv', delimiter=',')
+
+# get only the first 10 rows for data
+data = data.head(10)
+
+data[['Name', 'Address']] = data['md_VAT'].apply(lambda num: pd.Series(fetch_kbo_data(num)))
 
 # adding place_id from google
 data['place_id'] = data.apply(lambda row: get_place_id(row['Name'], row['Address']), axis=1)
 
 # Getting Google reviews
-data['reviews'] = data.apply(lambda row: get_details_from_places_api(row['place_id']), axis=1)
+#data['reviews'] = data.apply(lambda row: get_details_from_places_api(row['place_id']), axis=1)
 
 data
 
